8ca513154f1c4dae8271fe61553b51987d5106b2
Shekhar Ashok Mohite <122733304+shekharma@users.noreply.github.com>
2023-07-20 20:33:23 +0530

    Update and rename print_grid_search_attributes.py to fun.py

--- fun.py
+++ fun.py
@@ -1,3 +1,37 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+plt.rcParams['font.family']='DejaVu Sans'
+
+def plot_confusion_matrix(cm, classes,
+                         normalize=False,
+                         title='Confusion matrix',
+                         cmap=plt.cm.Blues):
+    if normalize:
+        cm=cm.astype('float')/cm.sum(axis=1)[:,np.newaxis]
+    
+    plt.imshow(cm,interpolation='nearest',cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks=np.arange(len(classes))
+    plt.xticks(tick_marks,classes,rotation=90)
+    plt.yticks(tick_marks,classes)
+    
+    fmt='.2f' if normalize else'd'
+    thresh=cm.max()/2
+    for i,j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+        plt.text(j,i, format(cm[i,j],fmt),
+                 horizontalalignment='center',
+                 color='white' if cm[i,j]>thresh else 'black')
+    
+    plt.tight_layout()
+    plt.ylabel('True lable')
+    plt.xlabel('Predicted label')
+
+
+
+
 from datetime import datetime
 def perform_model(model,x_train,y_train,x_test,y_test,class_labels,cm_normalize=True,\
                  print_cm=True, sm_cmap=plt.cm.Greens):
